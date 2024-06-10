9869e241c5971f98cd3bf0abb15ea93d527bb866
igrik723 <hilkewich7@gmail.com>
2024-06-10 22:42:42 +0300

    add types.ts

---
+++ src/app/types.ts
@@ -0,0 +1,39 @@
+export type User = {
+  id: number;
+  email: string;
+  role: string;
+  password: string;
+  name: string;
+  address: string;
+  phoneNumber: string;
+  createdAt: Date;
+  updatedAt: Date;
+  agreements: Agreement[];
+};
+
+export type Agreement = {
+  id: number;
+  user: User;
+  userId: number;
+  dateOfStart: Date;
+  dateOfEnd: Date;
+  sale: Sale[];
+};
+
+export type Sale = {
+  id: number;
+  agreement: Agreement;
+  agreementId: number;
+  furnitureModel?: FurnitureModel | null;
+  furnitureId: number;
+  count: number;
+};
+
+export type FurnitureModel = {
+  id: number;
+  sales: Sale[];
+  furnitureName: string;
+  furnitureType: string;
+  Property: string;
+  Price: number;
+};
