25816c7654369c08548f948d8731eab146815139
igrik723 <hilkewich7@gmail.com>
2024-06-29 15:19:11 +0300

    update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,25 +1,26 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query"
-import { BASE_URL } from "../../constants"
-import { RootState } from "../store"
+// services/api.ts
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../../constants';
+import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: `${BASE_URL}/api`,
-    prepareHeaders: (headers, { getState }) => {
-        const token = (getState() as RootState).auth.token || localStorage.getItem("token")
+  baseUrl: `${BASE_URL}/api`,
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).user.token || localStorage.getItem('token');
 
-        if (token) {
-            headers.set('authorization', `Bearer ${token}`)
-        }
-
-        return headers;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
     }
-})
 
-const baseQueryWithRetry = retry(baseQuery, {maxRetries: 1})
+    return headers;
+  },
+});
+
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 });
 
 export const api = createApi({
-    reducerPath: 'splitApi',
-    baseQuery: baseQueryWithRetry,
-    refetchOnMountOrArgChange: true,
-    endpoints: () => ({})
-})
+  reducerPath: 'splitApi',
+  baseQuery: baseQueryWithRetry,
+  refetchOnMountOrArgChange: true,
+  endpoints: () => ({}),
+});
