71326cf365805cc78f5df7aafb6f40fd697bc27a
igrik723 <hilkewich7@gmail.com>
2024-06-29 15:28:23 +0300

    other updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import MainPage from './pages/MainPage/MainPage';
+import { useDispatch } from 'react-redux';
+import { setUser } from './features/user/userSlice';
 
-function App() {
+const App: React.FC = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    const email = localStorage.getItem('email')
+    const name = localStorage.getItem('name')
+    const address = localStorage.getItem('address')
+    const phoneNumber = localStorage.getItem('phoneNumber')
+
+    if (token && email && name && address && phoneNumber) {
+      dispatch(setUser({ token, email, name, address, phoneNumber }))
+    }
+  }, [dispatch])
+
+
   return (
     <div className="App">
        <MainPage/>
--- src/components/UI/NavBar/NavBar.tsx
+++ src/components/UI/NavBar/NavBar.tsx
@@ -4,9 +4,29 @@ import basket from '../../../assets/корзина.png'
 import auth from '../../../assets/авторизация.png'
 import search from '../../../assets/поиск.png'
 import logo from '../../../assets/лого.jpg'
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
+import { closeAuthModal, closeBasketModal,  closeUserInfoModal, openAuthModal, openBasketModal, openUserInfoModal} from "../../../features/modal/modalSlice";
+import BasketModal from "../../Modal/BasketModal/BasketModal";
+import AuthModal from "../../Modal/AuthModal/AuthModal";
+import UserInfoModal from "../../Modal/UserInfoModal/UserInfoModal";
 
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
+    const dispathch = useDispatch()
+    const isAuthModalOpen = useSelector((state: RootState) => state.modal.isAuthModalOpen)
+    const isBasketModalOpen = useSelector((state: RootState) => state.modal.isBasketModalOpen)
+    const isUserInfoModalOpen = useSelector((state: RootState) => state.modal.isUserInfoModalOpen)
+    const user = useSelector((state: RootState) => state.user)
+
+    const handleAuthButtonClick = () => {
+        if (user.token) {
+            dispathch(openUserInfoModal())
+        } else {
+            dispathch(openAuthModal())
+        }
+    }
+
     return ( 
         <div
             className={styles.NavWrapper}
@@ -32,15 +52,29 @@ const NavBar = () => {
             <div
                 className={styles.NavLogos}
             >
-                <img
-                    src={basket}
-                    className={styles.basket}
-                />
-                <img
-                    src={auth}
-                    className={styles.auth}
-                />
+                <button
+                    className={styles.basketBtn}
+                    onClick={() => dispathch(openBasketModal())}
+                >
+                    <img
+                        src={basket}
+                        className={styles.basket}
+                    />
+                </button>
+                
+                <button
+                    className={styles.authBtn}
+                    onClick={handleAuthButtonClick}
+                >
+                    <img
+                        src={auth}
+                        className={styles.auth}
+                    />
+                </button>
             </div>
+            <BasketModal open={isBasketModalOpen} onClose={() => dispathch(closeBasketModal())} />
+            <AuthModal open={isAuthModalOpen} onClose={() => dispathch(closeAuthModal())} />
+            <UserInfoModal open={isUserInfoModalOpen} onClose={() => dispathch(closeUserInfoModal())} user={user}/>
         </div>
      );
 }
