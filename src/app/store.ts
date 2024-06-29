ef961864c2ab64da69ec53d5ac9b421cd2dd2abf
igrik723 <hilkewich7@gmail.com>
2024-06-29 15:17:42 +0300

    add store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,17 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./services/api";
+import modalReducer from "../features/modal/modalSlice";
+import userReducer from "../features/user/userSlice"
 
 export const store = configureStore({
     reducer: {
-        [api.reducerPath] : api.reducer
+        [api.reducerPath]: api.reducer,
+        modal: modalReducer,
+        user: userReducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware),
 })
 
 export type AppStore = typeof store
