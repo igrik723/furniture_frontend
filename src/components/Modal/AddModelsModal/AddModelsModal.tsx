422ad87606f8c334cc13f8536ff5a7d1d9e49230
igrik723 <hilkewich7@gmail.com>
2024-09-03 13:18:43 +0300

    update AddModelsModal

--- src/components/Modal/AddModelsModal/AddModelsModal.tsx
+++ src/components/Modal/AddModelsModal/AddModelsModal.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { Modal, Box, TextField, Button, Typography } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLoginMutation, useRegisterMutation } from '../../../app/services/userApi';
-import { setUser } from '../../../features/user/userSlice';
-import { useCreateModelMutation } from '../../../app/services/furnitureModelApi';
+import { Modal, Box, TextField, Button, Typography, Input } from '@mui/material';
+import axios from 'axios';
+import {useCreateModelMutation } from '../../../app/services/furnitureModelApi';
 
 interface AuthModalProps {
     open: boolean;
@@ -13,19 +11,41 @@ interface AuthModalProps {
 const AddModelsModal: React.FC<AuthModalProps> = ({ open, onClose }) => {
     const [nameModel, setNameModel] = useState('')
     const [typeModel, setTypeModel] = useState('')
-    const [propery, setProperty] = useState('')
+    const [property, setProperty] = useState('')
     const [price, setPrice] = useState('')
-    const [createModel] = useCreateModelMutation()
-    const dispatch = useDispatch();
+    const [count, setCount] = useState('')
+    const [image, setImage] = useState<File | null>(null);
+    const [createModel] = useCreateModelMutation();
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            setImage(e.target.files[0])
+        }
+    }
 
     const addModel = async () => {
-        const response = await createModel({ furnitureName: nameModel, furnitureType: typeModel, Property: propery, Price: Number(price) }).unwrap()
-        setNameModel('');
-        setTypeModel('')
-        setProperty('')
-        setPrice('')
-        onClose()
-    }
+        try {
+            await createModel({
+                furnitureName: nameModel,
+                furnitureType: typeModel,
+                Property: property,
+                Price: price,
+                count: count,
+                img: image
+            }).unwrap();
+
+            // Сбросить поля формы
+            setNameModel('');
+            setTypeModel('');
+            setProperty('');
+            setPrice('');
+            setCount('');
+            setImage(null);
+            onClose();
+        } catch (error) {
+            console.error('Error adding model:', error);
+        }
+    };
     
 
     return (
@@ -35,14 +55,39 @@ const AddModelsModal: React.FC<AuthModalProps> = ({ open, onClose }) => {
                     <Typography variant='h4'>Добавить модель</Typography>
                 </Box>
                 <Box>
+                    <Input
+                        type='file' 
+                        id='fileInput'
+                        onChange={handleImageChange}
+                        sx={{mb: 2}}
+                    />
                     <TextField label="Название" fullWidth margin="normal" value={nameModel} onChange={(e) => setNameModel(e.target.value)} />
                     <TextField label="Тип" fullWidth margin="normal" value={typeModel} onChange={(e) => setTypeModel(e.target.value)} />
-                    <TextField label="Свойства"  fullWidth margin="normal" value={propery} onChange={(e) => setProperty(e.target.value)} />
+                    <TextField label="Свойства"  fullWidth margin="normal" value={property} onChange={(e) => setProperty(e.target.value)} />
                     <TextField
                         label="Цена"
                         type="number"
                         value={price}
                         onChange={(e) => setPrice(e.target.value)}
+                        sx={{
+                            '& input[type=number]': {
+                                '-moz-appearance': 'textfield',
+                            },
+                            '& input[type=number]::-webkit-outer-spin-button': {
+                                '-webkit-appearance': 'none',
+                                margin: 0,
+                            },
+                            '& input[type=number]::-webkit-inner-spin-button': {
+                                '-webkit-appearance': 'none',
+                                margin: 0,
+                            },
+                        }}
+                    />
+                    <TextField
+                        label="Кол-во"
+                        type="number"
+                        value={count}
+                        onChange={(e) => setCount(e.target.value)}
                         sx={{
                             '& input[type=number]': {
                                 '-moz-appearance': 'textfield',
