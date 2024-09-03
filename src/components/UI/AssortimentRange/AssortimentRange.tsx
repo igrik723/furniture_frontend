fa08a3733477f2c54e611d3b28d53c6a9307f4ba
igrik723 <hilkewich7@gmail.com>
2024-09-03 13:20:05 +0300

    update AssortimentRange

--- src/components/UI/AssortimentRange/AssortimentRange.tsx
+++ src/components/UI/AssortimentRange/AssortimentRange.tsx
@@ -3,8 +3,11 @@ import styles from './AssortimentRange.module.css'
 import table from '../../../assets/стол.png'
 import cabinet from '../../../assets/тумбы.png'
 import cupboard from '../../../assets/шкафы.png'
+import { useNavigate } from "react-router-dom";
 
 const AssortimentRange: React.FC = () => {
+    const navigate = useNavigate()
+
     return ( 
         <div
             className={styles.wrapper}
@@ -18,18 +21,21 @@ const AssortimentRange: React.FC = () => {
                 className={styles.imagesWrapper}
             >   
                 <div
+                    onClick={() => navigate('/table') }
                     className={styles.imageWrapper}
                 >
                     <img className={styles.table} src={table} alt="table"/>
                     <h4 className={styles.subTitle}>Столы</h4>
                 </div>
                 <div
+                    onClick={() => navigate('/cabinet')}
                     className={styles.imageWrapper}
                 >
                     <img className={styles.cabinet} src={cabinet} alt="cabinet"/>
                     <h4 className={styles.subTitle}>Тумбы</h4>
                 </div>
                 <div
+                    onClick={() => navigate('/cupboard')}
                     className={styles.imageWrapper}
                 >
                     <img className={styles.cupboard} src={cupboard} alt="cupboard"/>
