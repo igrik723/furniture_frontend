8cf45428ca28f0a189fde43c07f527ee59222583
igrik723 <hilkewich7@gmail.com>
2024-09-03 13:22:31 +0300

    update MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -11,7 +11,6 @@ const MainPage = () => {
         <div
             className={styles.wrapper}
         >
-            <NavBar />
             <ImageSlider />
             <AssortimentRange />
             <Advantages />
