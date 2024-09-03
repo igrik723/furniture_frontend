1c7ab6ecfe1276e01ac1dc3f7ed94a1da9a8937b
igrik723 <hilkewich7@gmail.com>
2024-09-03 13:18:05 +0300

    update FurnitureModelApi

--- src/app/services/furnitureModelApi.ts
+++ src/app/services/furnitureModelApi.ts
@@ -1,39 +1,72 @@
-import { Search } from "@mui/icons-material";
 import { FurnitureModel } from "../types";
 import { api } from "./api";
 
 export const furnitureModelApi = api.injectEndpoints({
     endpoints: (builder) => ({
         createModel: builder.mutation<
-            FurnitureModel, { furnitureName: string; furnitureType: string; Property: string, Price: number }
+            FurnitureModel, 
+            { 
+                furnitureName: string; 
+                furnitureType: string; 
+                Property: string; 
+                Price: string;
+                count: string;
+                img: File | null  
+            }
         >({
-            query: (modelData) => ({
-                url: '/furnitureModels',
-                method: 'POST',
-                body: modelData
-            })
+            query: ({ furnitureName, furnitureType, Property, Price, img, count }) => {
+                const formData = new FormData();
+                formData.append('furnitureName', furnitureName);
+                formData.append('furnitureType', furnitureType);
+                formData.append('Property', Property);
+                formData.append('Price', Price);
+                formData.append('count', count)
+                if (img) {
+                    formData.append('img', img);
+                }
+                return {
+                    url: '/furnitureModels',
+                    method: 'POST',
+                    body: formData,
+                };
+            }
         }),
-          deleteModel: builder.mutation<void, string>({
-              query: (id) => ({
-                  url: `/furnitureModels/${id}`,
-                  method: 'DELETE'
-              })
-          }),
-          searchModels: builder.query<FurnitureModel[], string>({
-              query: (search) => ({
+        deleteModel: builder.mutation<void, number>({
+            query: (id) => ({
+                url: `/furnitureModels/${id}`,
+                method: 'DELETE',
+            }),
+        }),
+        searchModels: builder.query<FurnitureModel[], string>({
+            query: (search) => ({
                 url: `/furnitureModels?search=${search}`,
-                  method: 'GET'
-              })
-          })
-    })
-})
+                method: 'GET',
+            }),
+        }),
+        updateModelCount: builder.mutation<
+            void, 
+            {
+                id: number,
+                count: string,
+            }
+            >({
+                query: ({ id, count }) => ({
+                url: `/furnitureModels/${id}`,
+                method: 'PUT',
+                body: { count },
+            })
+        })
+
+    }),
+});
 
 export const {
     useCreateModelMutation,
     useDeleteModelMutation,
-    useSearchModelsQuery
-} = furnitureModelApi
+    useSearchModelsQuery,
+    useUpdateModelCountMutation,
+} = furnitureModelApi;
 
 export const {
-    endpoints: {createModel, deleteModel}
-} = furnitureModelApi
+    endpoints: { createModel, deleteModel },
+} = furnitureModelApi;
